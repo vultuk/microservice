f2f9524407c07950582384a4e0389a0528f96761
Simon Skinner <vultuk28@gmail.com>
2021-03-11 15:23:05 +0000

    Exported types and lodash

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,8 @@ import _ from './Shared/Lodash';
 import {Route} from './Types/Route';
 import {Settings} from './Types/Settings';
 
+export * from './Types';
+export * as lodash from './Shared/Lodash';
+
 export const Microservice = (settings?: Settings) => (middleware?: any[]) => (routes: Route[]) =>
   _.pipe(Setup(settings || {}), Middleware(middleware), Routes(routes), ErrorHandling, Serve);
