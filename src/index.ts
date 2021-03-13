22ba9be06c95e6f18258b9152f1e7efecc601a61
Simon Skinner <vultuk28@gmail.com>
2021-03-13 07:12:05 +0000

    Lodash export

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,12 @@ import Middleware from './Server/Middleware';
 import Routes from './Server/Routes';
 import Serve from './Server/Serve';
 import Setup from './Server/Setup';
-import _ from './Shared/Lodash';
-import { Route } from './Types/Route';
-import { Settings } from './Types/Settings';
+import lodash from './Shared/Lodash';
+import {Route} from './Types/Route';
+import {Settings} from './Types/Settings';
 
 export * from './Types';
-export * as lodash from './Shared/Lodash';
+export const _ = lodash;
 
 export const Microservice = (settings?: Settings) => (middleware?: any[]) => (routes: Route[]) =>
   _.pipe(Setup(settings || {}), Middleware(middleware), Routes(routes), ErrorHandling, Serve);
