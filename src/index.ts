e0f69f57a4c1f58a55a0619a219d98a2ca46a615
Simon Skinner <vultuk28@gmail.com>
2021-04-05 18:41:20 +0100

    Updated NPM name

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Request as ExpressRequest, Response as ExpressResponse } from 'express';
+import {Request as ExpressRequest, Response as ExpressResponse} from 'express';
 
 import ErrorHandling from './Server/ErrorHandling';
 import Middleware from './Server/Middleware';
@@ -6,8 +6,8 @@ import Routes from './Server/Routes';
 import Serve from './Server/Serve';
 import Setup from './Server/Setup';
 import lodash from './Shared/Lodash';
-import { Route } from './Types/Route';
-import { Settings } from './Types/Settings';
+import {Route} from './Types/Route';
+import {Settings} from './Types/Settings';
 
 export * from './Types';
 export const _ = lodash;
