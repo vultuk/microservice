dad2a04614ebf9d3db1b0bb691c093131330a96f
Simon Skinner <simon@vult.uk>
2021-05-05 08:38:35 +0100

    Work

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Request as ExpressRequest, Response as ExpressResponse } from 'express';
+import {Request as ExpressRequest, Response as ExpressResponse} from 'express';
 
 import ErrorHandling from './Server/ErrorHandling';
 import Middleware from './Server/Middleware';
@@ -6,13 +6,14 @@ import Routes from './Server/Routes';
 import Serve from './Server/Serve';
 import Setup from './Server/Setup';
 import lodash from './Shared/Lodash';
-import { Route } from './Types/Route';
-import { Settings } from './Types/Settings';
+import {Route} from './Types/Route';
+import {Settings} from './Types/Settings';
 
 export * from './Types';
 export const _ = lodash;
 export type Request = ExpressRequest;
 export type Response = ExpressResponse;
 
-export const Microservice = (settings?: Settings) => (middleware?: any[]) => (routes: Route[]) =>
-  _.pipe(Setup(settings || {}), Middleware(middleware), Routes(routes), ErrorHandling, Serve);
+export const Microservice = (settings?: Settings) => (middleware?: any[], appOnlyMiddleware?: any[]) => (
+  routes: Route[],
+) => _.pipe(Setup(settings || {}), Middleware(middleware), Routes(routes), ErrorHandling, Serve);
