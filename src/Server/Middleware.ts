a8ffc24d83b9a6dcd5f9328e7d400bc1e9a13d75
Simon Skinner <simon@vult.uk>
2021-05-05 08:48:41 +0100

    Work

--- src/Server/Middleware.ts
+++ src/Server/Middleware.ts
@@ -3,7 +3,8 @@ import bodyParser from 'body-parser';
 import {SetupResults} from '../Types/SetupResults';
 
 export default (middleware?: any[], appOnlyMiddleware: any[] = []) => (setup: SetupResults): SetupResults => {
-  let { settings, app } = setup;
+  let { app } = setup;
+  const { settings } = setup;
 
   [bodyParser.json(), ...(middleware || [])].forEach((item) => app.use(item));
   appOnlyMiddleware.forEach((item) => (app = item(app)));
