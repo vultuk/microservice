51695a5a0e55590f37b8de01dd9acbed94ce87a7
Simon Skinner <simon@vult.uk>
2021-05-05 08:29:33 +0100

    App only middleware

--- src/Server/Middleware.ts
+++ src/Server/Middleware.ts
@@ -1,11 +1,12 @@
 import bodyParser from 'body-parser';
 
-import { SetupResults } from '../Types/SetupResults';
+import {SetupResults} from '../Types/SetupResults';
 
-export default (middleware?: any[]) => (setup: SetupResults): SetupResults => {
+export default (middleware?: any[], appOnlyMiddleware: any[] = []) => (setup: SetupResults): SetupResults => {
   const { settings, app } = setup;
 
   [bodyParser.json(), ...(middleware || [])].forEach((item) => app.use(item));
+  appOnlyMiddleware.forEach((item) => item(app));
 
   return { settings, app };
 };
