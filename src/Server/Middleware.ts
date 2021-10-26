39550405e1aaf414924c2b12c34994f3d6e47990
Simon Skinner <vultuk28@gmail.com>
2021-10-26 15:30:31 +0000

    Making is possible to set the bodyParser JSON limit

--- src/Server/Middleware.ts
+++ src/Server/Middleware.ts
@@ -1,11 +1,12 @@
 import bodyParser from 'body-parser';
+import { Settings } from '../Types/Settings';
 
 import { SetupResults } from '../Types/SetupResults';
 
-export default (middleware?: any[], appOnlyMiddleware: any[] = []) => (setup: SetupResults): SetupResults => {
+export default (middleware?: any[], appOnlyMiddleware: any[] = [], settings: Settings = {}) => (setup: SetupResults): SetupResults => {
   const { settings, app } = setup;
 
-  [bodyParser.json(), ...(middleware || [])].forEach((item) => app.use(item));
+  [bodyParser.json({limit: settings.bodySize ?? "100kb"}), ...(middleware || [])].forEach((item) => app.use(item));
   appOnlyMiddleware.forEach((item) => item(app));
 
   return { settings, app };
